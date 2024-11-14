309979085242b1c62774c9eb59bc60c58dbebbea
Sandhavi Wanigasooriya <sandhaviwanigasooriya@gmail.com>
2024-11-14 13:36:46 +0530

    Updated

--- Controller.php
+++ Controller.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace app\core;
+
 use app\core\middlewares\BaseMiddleware;
 
 /**
@@ -14,16 +15,16 @@ class Controller
 	 * @var \app\core\middlewares\BaseMiddleware[]
 	 */
 	protected array $middlewares = [];
-    public string $layout = 'main';
+	public string $layout = 'main';
 	public string $action = '';
-    public function setLayout($layout)
-    {
-        $this->layout = $layout;
-    }
-    public function render($view, $params = [])
-    {
-        return Application::$app->view->renderView($view, $params);
-    }
+	public function setLayout($layout)
+	{
+		$this->layout = $layout;
+	}
+	public function render($view, $params = [])
+	{
+		return Application::$app->view->renderView($view, $params);
+	}
 	public function registerMiddleware(BaseMiddleware $middleware)
 	{
 		$this->middlewares[] = $middleware;
