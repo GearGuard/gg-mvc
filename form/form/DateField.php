34cfed969a7dcf5ed7d74aa87872078350626790
Sandhavi Wanigasooriya <sandhaviwanigasooriya@gmail.com>
2025-04-27 12:23:08 +0530

    Refactor form field classes by removing unused files and consolidating functionality into BaseField

--- form/form/DateField.php
+++ form/form/DateField.php
@@ -9,6 +9,8 @@ class DateField extends BaseField
     public bool $readonly = false;
     public bool $disabled = false;
     public string $placeholder = '';
+    public string $min = '';
+    public string $max = '';
 
     public function required(bool $required = true): self
     {
@@ -34,13 +36,33 @@ class DateField extends BaseField
         return $this;
     }
 
+    public function min(string $min): self
+    {
+        if (!preg_match('/^\d{4}-\d{2}-\d{2}$/', $min)) {
+            throw new \InvalidArgumentException('Invalid date format. Use YYYY-MM-DD.');
+        }
+        $this->min = $min;
+        return $this;
+    }
+
+    public function max(string $max): self
+    {
+        if (!preg_match('/^\d{4}-\d{2}-\d{2}$/', $max)) {
+            throw new \InvalidArgumentException('Invalid date format. Use YYYY-MM-DD.');
+        }
+        $this->max = $max;
+        return $this;
+    }
+
     public function renderInput(): string
     {
         return sprintf(
-            '<input type="date" id="%s" name="%s" value="%s" placeholder="%s" class="form-input%s" %s %s %s>',
+            '<input type="date" id="%s" name="%s" value="%s" %s %s placeholder="%s" class="form-input%s" %s %s %s>',
             $this->attribute,
             $this->attribute,
             $this->model->{$this->attribute},
+            $this->min ? 'min="' . $this->min . '"' : '',
+            $this->max ? 'max="' . $this->max . '"' : '',
             $this->placeholder == '' ? "Enter the " . $this->model->getLabel($this->attribute): $this->placeholder,
             $this->model->hasError($this->attribute) ? ' is-invalid' : '',
             $this->readonly ? ' readonly' : '',
